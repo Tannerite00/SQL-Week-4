4647b5ec45aeb3a12aad1185fe195541f19d38af
Tanner Dodd <31601567+Tannerite00@users.noreply.github.com>
2021-09-07 13:34:03 -0600

    Update AnimalMain.java

--- Week 4 SQL assignment/src/animal/AnimalMain.java
+++ Week 4 SQL assignment/src/animal/AnimalMain.java
@@ -7,5 +7,9 @@ public class AnimalMain {
 		public static void main(String [] args) {
 			List<Animals> sortedAnimalList = AnimalSort.sortAnimals();
 			System.out.println(sortedAnimalList);
+			
+			List<Animals> methodAnimalList = AnimalSort.methodReference();
+			System.out.println(methodAnimalList);
+			
 		}
 }
